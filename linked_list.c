28f53a325aa9de63895f14cd9a04d4fc7ef4292b
Rebecca Jeon <99245606+rebecca-jeon@users.noreply.github.com>
2022-10-09 23:09:48 -0700

    Assignment 1

---
+++ linked_list.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <errno.h>
+#include "linked_list.h"
+
+ 
+Node * add_newNode(Node* head, pid_t new_pid, char * new_path){
+	// Your code here
+	return NULL; // Change this, DO NOT return NULL !
+}
+
+
+Node * deleteNode(Node* head, pid_t pid){
+	// your code here
+	return head;
+}
+
+void printList(Node *node){
+	// your code here
+}
+
+
+int PifExist(Node *node, pid_t pid){
+	// your code here
+  	return 0;
+}
+
